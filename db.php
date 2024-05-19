c5466eea8670121695d7af84c92af84440218b50
EugenioMacSwiney <124908483+EugenioMacSwiney@users.noreply.github.com>
2024-05-18 18:21:43 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,12 @@
 <?php
-
+ob_start(); 
 $DB_HOST = $_ENV["DB_HOST"];
 $DB_USER = $_ENV["DB_USER"];
 $DB_PASSWORD = $_ENV["DB_PASSWORD"];
 $DB_NAME = $_ENV["DB_NAME"];
 $DB_PORT = $_ENV["DB_PORT"];
 
-$conexion = mysqli_connect("$DB_HOST:$DB_PORT", "$DB_USER", "$DB_PASSWORD", "$DB_NAME") or die("Error de conexión: " . mysqli_connect_error());
+
+$conexion=mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT")or die("error conexion");
+?>
+<!--  -->
