e58b163eb82b85a9745fb8bf9060fd2d09e2f658
EugenioMacSwiney <124908483+EugenioMacSwiney@users.noreply.github.com>
2024-05-18 18:14:19 -0600

    Update eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -4,16 +4,20 @@ include('/app/db.php');
 
 // Escapar el valor de ID para prevenir inyección SQL
 $ID = mysqli_real_escape_string($conexion, $_POST['txtID']);
+echo "ID a eliminar: " . $ID . "<br>"; // Declaración de depuración
 
 // Preparar la consulta SQL
 $query = "DELETE FROM usuarios WHERE ID = ?";
+echo "Consulta SQL: " . $query . "<br>"; // Declaración de depuración
 $stmt = mysqli_prepare($conexion, $query);
 
 // Vincular el parámetro
 mysqli_stmt_bind_param($stmt, "s", $ID);
+echo "Parámetro vinculado<br>"; // Declaración de depuración
 
 // Ejecutar la consulta
 if (mysqli_stmt_execute($stmt)) {
+    echo "Consulta ejecutada correctamente<br>"; // Declaración de depuración
     // Eliminar exitoso
     mysqli_stmt_close($stmt);
     mysqli_close($conexion);
@@ -22,7 +26,7 @@ if (mysqli_stmt_execute($stmt)) {
     exit();
 } else {
     // Error al eliminar
-    echo "Error al eliminar: " . mysqli_stmt_error($stmt);
+    echo "Error al eliminar: " . mysqli_stmt_error($stmt) . "<br>"; // Declaración de depuración
     mysqli_stmt_close($stmt);
     mysqli_close($conexion);
 }
